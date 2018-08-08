e6b0f5c0532ea7e631e2caee14dec55ef94c5313
Johannes Wünsche <johannes.wuensche97@gmail.com>
2018-08-08 15:53:00 +0200

    Add Close, GetStatus and SendMessage functionality

--- connection.go
+++ connection.go
@@ -2,8 +2,10 @@ package sturdyengine
 
 import (
 	"flag"
+	"fmt"
 	"net/url"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/websocket"
 )
 
@@ -25,6 +27,57 @@ func (conn *Connection) InitializeAPI(clientName string) (e error) {
 
 	c, _, e := websocket.DefaultDialer.Dial(conn.Uri.String(), nil)
 	conn.Conn = c
+
+	return
+}
+
+func (conn *Connection) Close() (e error) {
+
+	e = conn.Conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if e != nil {
+		return
+	}
+
+	return
+}
+
+//GetStatus calls procedure `GetStatus` and returns the response in form of the proto buffer message
+func (conn *Connection) GetStatus() (res Response, e error) {
+
+	pc := ProcedureCall{
+		Service:   "KRPC",
+		Procedure: "GetStatus",
+	}
+	pr := Request{
+		Calls: []*ProcedureCall{&pc},
+	}
+
+	res, e = conn.sendMessage(pr)
+
+	fmt.Println(res.String())
+
+	e = nil
+	return
+}
+
+func (conn *Connection) sendMessage(r Request) (res Response, e error) {
+	req, e := proto.Marshal(&r)
+
+	if e != nil {
+		return
+	}
+
+	conn.Conn.WriteMessage(websocket.BinaryMessage, req)
+
+	_, p, e := conn.Conn.ReadMessage()
+	if e != nil {
+		return
+	}
+	res = Response{}
+	e = proto.Unmarshal(p, &res)
+	if e != nil {
+		return
+	}
 
 	return
 }
